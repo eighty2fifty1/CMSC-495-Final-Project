9d3b2abe410c8934bda21d9f6a989f7f1eac52e1
eighty2fifty1 <huskernavy70@gmail.com>
2024-03-26 23:17:39 -0700

    (mostly) functional complete app

--- static/js/login.js
+++ static/js/login.js
@@ -1,1 +1,17 @@
-document.write("Hello World");
+var socket = io.connect('http://localhost:5000');
+
+
+function login(){
+    console.log("Login called");
+}
+
+//checks db for user ID in use
+//if not already in, loads signup page
+function signUp(userID){
+    if (userID != null){
+        document.write("User ID in use");
+    }
+    else{
+        //navigate to signup page
+    }
+}
