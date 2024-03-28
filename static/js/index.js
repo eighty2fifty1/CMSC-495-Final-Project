d29b365e570b75ccdb1dffc314d3cec5f87cfd9d
eighty2fifty1 <huskernavy70@gmail.com>
2024-03-28 16:37:50 -0700

    more front end work

--- static/js/index.js
+++ static/js/index.js
@@ -1,16 +1,33 @@
 var socket = io.connect('http://localhost:5000');
+var userID;
+var activeContact;
+var messageTemplate = new Object();
 
+/*****************************
+ * 
+ * Handling incoming signals
+ * 
+ *****************************/
 
 socket.on('connect', function () {
   console.log('Websocket connected!');
 });
 
-socket.on('message', function (msg) {
-  var li = document.createElement("li");
-  li.textContent = msg;
-  document.getElementById("chat").appendChild(li);
-});
+socket.on('message', incomingMsg(message));
 
+function incomingMsg(msg){
+  const msgBox = document.createElement("li");
+  msgBox.classList.add("w3-container");
+  msgBox.classList.add("msg");
+  console.log(msg);
+  msgBox.textContent = msg.timestamp + " " + msg.content;
+  if(msg.sender == userID){
+    msgBox.classList.add("outgoing");
+  } else{
+    msgBox.classList.add("incoming");
+  }
+  document.getElementById("chat-list").appendChild(msgBox);
+}
 
 //pulls list of contacts and populates tabs with message history
 function initializeContacts() {
@@ -19,26 +36,46 @@ function initializeContacts() {
 
 function sendMessage() {
   console.log('sendMessage called');  // For debugging
-  var input = document.getElementById("messageInput");
-  var message = input.value;
-  socket.emit('message', message);
-  input.value = '';
+  var input = document.getElementById("send-box");
+  messageTemplate.timestamp = Date.now();  //sender, receiver, timestamp, content
+  messageTemplate.sender = userID;
+  messageTemplate.receiver = activeContact;
+  messageTemplate.content = input.innerText;
+  var msgJSON = JSON.stringify(messageTemplate);
+  console.log(messageTemplate);
+  socket.emit('message', messageTemplate);
+  input.innerText = '';
+  incomingMsg(messageTemplate);
 }
 
-function openChat(evt, userID) {
-  var i, x, tablinks;
-  x = document.getElementsByClassName("city");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+
+//called when contact button is clicked
+function openChatTab(evt, contact) {  var i, x, tablinks;
+    x = document.getElementById("chat-list");
+    //clear list
+    while(x.firstChild){
+      x.removeChild(x.firstChild);
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" w3-red", ""); 
+    }
+    //code to show chat
+    //
+                                    
+    contact.chatLog.forEach(msg => {
+      incomingMsg(msg);
+    })                               
+    console.log(contact.name);
+    activeContact = contact.name;
+    evt.currentTarget.className += " w3-red";
   }
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < x.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
-  }
-  //code to show chat
-  //
-  evt.currentTarget.className += " w3-red";
-}
+
+/******************************************
+ * 
+ *  pulls json data for front end testing
+ * 
+ ******************************************/
 
 async function fetchDummyChat() {
   fetch('http://localhost:5000/json')
@@ -51,6 +88,7 @@ async function fetchDummyChat() {
     .then(data => {
       // Extract the 'name' from the 'data' object
       const users = JSON.parse(data);
+      userID = users.username;
       console.log(users.contacts[0].name);
       tablinks = document.getElementsByClassName("tablink");
       users.contacts.forEach(contact => {
@@ -58,7 +96,7 @@ async function fetchDummyChat() {
         userButton.className += "w3-bar-item";
         userButton.classList.add("w3-button");
         userButton.classList.add("tablink");
-        userButton.addEventListener("click", function () { openChat("click", contact.name); })  //needs work. try reorganizing args or inserting html code directly
+        userButton.addEventListener("click", function () { openChatTab("click", contact); })  //needs work. try reorganizing args or inserting html code directly
         userButton.textContent = contact.name;
         document.getElementById("sidebar").appendChild(userButton);
       })
