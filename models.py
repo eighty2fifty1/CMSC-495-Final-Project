77dcd8c5b98876df83d686fca36d3602440e2dde
gomamishra12 <164284619+gomamishra12@users.noreply.github.com>
2024-03-31 21:17:29 -0400

    Register, Login and Logout functionality
    Register, Login and Logout functionality

--- models.py
+++ models.py
@@ -1,13 +1,12 @@
 # models.py
 from extensions import db
 
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-
+    
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
 
@@ -15,10 +14,7 @@ class User(db.Model):
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String(500), nullable=False)
-    timestamp = db.Column(db.DateTime, index=True,
-                          default=db.func.current_timestamp())
-    sender = db.Column(db.String(50))
-    receiver = db.Column(db.String(50))
+    timestamp = db.Column(db.DateTime, index=True, default=db.func.current_timestamp())
 
     def __repr__(self):
         return f'<Message {self.message}>'
