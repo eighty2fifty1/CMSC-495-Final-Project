1d18599e813cf1414b678eb008190a0a405934f4
eighty2fifty1 <huskernavy70@gmail.com>
2024-04-09 20:32:11 -0700

    integrating full app

--- models.py
+++ models.py
@@ -1,26 +1,24 @@
 # models.py
 from extensions import db
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    
+
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
 
 
-class User(db.Model):
-    id = db.Column(db.String, primary_key=True)
-    password = db.Column(db.String)
-    email_addr = db.Column(db.String)
-    #contacts =
-
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String(500), nullable=False)
-    timestamp = db.Column(db.DateTime, index=True, default=db.func.current_timestamp())
+    timestamp = db.Column(db.DateTime, index=True,
+                          default=db.func.current_timestamp())
+    sender = db.Column(db.String(50))
+    receiver = db.Column(db.String(50))
 
     def __repr__(self):
         return f'<Message {self.message}>'
